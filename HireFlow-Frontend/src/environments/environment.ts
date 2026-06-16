e35aabea94629ef966f46593470048cd7ba78144
AMRIT PAL SINGH <146010078+gitamrit1443@users.noreply.github.com>
2026-06-16 17:12:05 +0530

    Update API URLs in environment configuration
    Removed trailing slashes from API URLs.

--- HireFlow-Frontend/src/environments/environment.ts
+++ HireFlow-Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
- apiUrl: 'https://hireflow-api-amrit.azurewebsites.net/',
-  plagiarismServiceUrl: 'https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/',
+ apiUrl: 'https://hireflow-api-amrit.azurewebsites.net',
+  plagiarismServiceUrl: 'https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net',
 };
