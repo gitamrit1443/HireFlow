0f04dfc10bfdcb5818010657ce6436e3776e5dfd
gitamrit1443 <amritpal.singh2023@outlook.com>
2026-07-14 01:25:49 +0530

    Deploying Project

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: `https://localhost:7001/api`,
+  apiUrl: `hireflow-production-3439.up.railway.app`,
   plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/`
 };
