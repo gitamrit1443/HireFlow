8c539fe62aa457224653e10422468b59f687e82f
gitamrit1443 <amritpal.singh2023@outlook.com>
2026-07-14 09:12:22 +0530

    Update Cors Files

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: `hireflow-production-3439.up.railway.app`,
+  apiUrl: `hireflow-production-3439.up.railway.app/api`,
   plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/`
 };
