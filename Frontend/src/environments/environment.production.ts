79e60e1aab6cf7d7f350f3ac036fdb957f1c3d1d
gitamrit1443 <amritpal.singh2023@outlook.com>
2026-07-14 09:39:37 +0530

    Update Cors Files

--- Frontend/src/environments/environment.production.ts
+++ Frontend/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: `hireflow-production-3439.up.railway.app/api`,
-  plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/`
+  apiUrl: `https://hireflow-production-3439.up.railway.app/api`,
+  plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/api`
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: `hireflow-production-3439.up.railway.app/api`,
-  plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/`
+  apiUrl: `https://hireflow-production-3439.up.railway.app/api`,
+  plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/api`
 };
