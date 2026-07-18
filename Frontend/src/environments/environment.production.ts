9d6ada49dc08d86821ea652bd72bd0d013c2fb54
AMRIT PAL SINGH <146010078+gitamrit1443@users.noreply.github.com>
2026-07-18 13:33:46 +0530

    Remove trailing slash from apiUrl

--- Frontend/src/environments/environment.production.ts
+++ Frontend/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: `https://hireflow-x8w5.onrender.com/`,
+  apiUrl: `https://hireflow-x8w5.onrender.com`,
   plagiarismServiceUrl: `https://hireflow-plagiarism-dja8hgabdjfdbqhg.malaysiawest-01.azurewebsites.net/api`
 };
